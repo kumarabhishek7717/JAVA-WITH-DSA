770456bf6411581f54ec5518351f61345eab1121
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-12-23 17:08:36 +0530

    done

---
+++ JAVADASA/Bubble.java
@@ -0,0 +1,123 @@
+public class Bubble {
+
+    public static void main(String[] args) {
+        int arr [] = {1,-2,3,-4,4,4,5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j =0 ; j< arr.length-1-i; j++){
+
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+            
+        }
+
+    }
+    
+}
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        int arr [] = {-1,-2,-3,-4,-4,-4,-5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j =0 ; j< arr.length-1-i; j++){
+                
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+            
+        }
+
+    }
+    
+}
+
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        int arr [] = {-1,-2,3,-4,4,-4,5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j =0 ; j< arr.length-1-i; j++){
+                
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+            
+        }
+
+    }
+    
+}
+
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        int arr [] = {1,-2,3,-4,-4,4,-5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j =0 ; j< arr.length-1-i; j++){
+                
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+            
+        }
+
+    }
+    
+}
+
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        int arr [] = {1,-7,3,-4,4,5};
+
+        for(int i = 0; i< arr.length-1; i++){
+            for(int j =0 ; j< arr.length-1-i; j++){
+                
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+            
+        }
+
+    }
+    
+}
+
